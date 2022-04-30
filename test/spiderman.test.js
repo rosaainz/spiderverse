98423aab905d11cadc00c85f9bec7416993e6816
rosaainz <al2173040136@azc.uam.mx>
2022-04-30 00:09:09 -0500

    8. Se escribio prueba deseada - Se provoca fallo al no llamar la clase

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -1,6 +1,17 @@
-describe("Test Suite Dummy Description", () => {
-    test('Case 1 Dummy', () => {
-      const resultOfSomething = 1 + 2
-      expect(resultOfSomething).toBe(3);
+const Spiderman = require("../app/Spiderman");
+
+describe("Unit Test for Spiderman class", () => {
+    test('1) Create an spiderman object', () => {
+        
+        //Codigo que queremos tal cual
+        //Queremos instanciar un objeto Spiderman con as sig info:
+        const andrewGarfield = new Spiderman("Spiderman Sony", 31, "Andrew Garfield", 2, "Sony")
+
+        //Validamos que el codigo funcione
+        expect(andrewGarfield.name).toBe("Spiderman Sony")
+        expect(andrewGarfield.age).toBe(31)
+        expect(andrewGarfield.actor).toBe("Andrew Garfield")
+        expect(andrewGarfield.movies).toBe(31)
+        expect(andrewGarfield.studio).toBe("Sony")
     });
   })
