7013bcde7103364b1a6d42737cd62361790888e2
rosaainz <al2173040136@azc.uam.mx>
2022-04-30 00:01:14 -0500

    7. Creacion de clase Spiderman con constructor

---
+++ app/Spiderman.js
@@ -0,0 +1,13 @@
+class Spiderman {
+
+    constructor(name, age, actor, numberFilms, filmStudio ){
+        this.name = name
+        this.age = age
+        this.actor = actor
+        this.numberFilms = numberFilms
+        this.filmStudio = filmStudio
+    }
+}
+
+//exporta la clase
+module.exports = Spiderman
